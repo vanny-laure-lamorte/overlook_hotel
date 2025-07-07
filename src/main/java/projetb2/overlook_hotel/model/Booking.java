b0ae49d29eac3a5469ebd4641b646730814d03b2
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-07-07 09:52:43 +0200

    ✨ <feat> add files for feedback form.

--- src/main/java/projetb2/overlook_hotel/model/Booking.java
+++ src/main/java/projetb2/overlook_hotel/model/Booking.java
@@ -19,8 +19,8 @@ public class Booking {
     private Integer id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    private HotelUser user;
+    @JoinColumn(name = "customer_id")
+    private HotelUser hotelUser;
 
     @ManyToOne
     @JoinColumn(name = "room_id", referencedColumnName = "id")
