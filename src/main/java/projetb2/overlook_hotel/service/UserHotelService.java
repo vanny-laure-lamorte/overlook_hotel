dfd8c9d1d5392216a41cae02496e3e2ecf586d0b
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-07-09 16:53:57 +0200

    ✨ <feat> edit input from profile.

--- src/main/java/projetb2/overlook_hotel/service/UserHotelService.java
+++ src/main/java/projetb2/overlook_hotel/service/UserHotelService.java
@@ -6,6 +6,7 @@ import projetb2.overlook_hotel.model.HotelUser;
 import projetb2.overlook_hotel.repository.HotelUserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserHotelService {
@@ -17,7 +18,27 @@ public class UserHotelService {
         return userHotelRepository.findAll();
     }
 
+    public HotelUser saveUser(HotelUser user) {
+        return userHotelRepository.save(user);
+    }
 
+    public HotelUser updateUser(HotelUser user) {
+        return userHotelRepository.save(user);
+    }
 
-    
+    public Optional<HotelUser> findByEmail(String email) {
+        return userHotelRepository.findByEmail(email);
+    }
+
+    public void deleteUser(HotelUser user) {
+        userHotelRepository.delete(user);
+    }
+
+    public List<HotelUser> findByRole(String roleName) {
+        return userHotelRepository.findByRole_RoleName(roleName);
+    }
+
+    public HotelUser findById(Integer id) {
+        return userHotelRepository.findById(id).orElse(null);
+    }
 }
