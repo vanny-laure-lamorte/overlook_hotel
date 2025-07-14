c85a3131519640548bb7f42e202b3dc03573f21d
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2025-07-14 22:02:52 +0200

    ✨ <feat> link loyalty page to navbar.

--- src/main/java/projetb2/overlook_hotel/controller/view/BookingViewController.java
+++ src/main/java/projetb2/overlook_hotel/controller/view/BookingViewController.java
@@ -74,24 +74,4 @@ public class BookingViewController {
             }
         return "layout/connectedLayout";
     }
-
-    @GetMapping("/loyalty-points")
-    public String showLoyaltyPoints(
-        Model model,
-        @AuthenticationPrincipal UserDetails currentUser) {
-
-        Optional<HotelUser> userOpt = hotelUserService.findByEmail(currentUser.getUsername());
-
-        model.addAttribute("fragmentPath", "fragments/past-booking");
-        model.addAttribute("fragmentName", "fgt-past-booking");
-
-        if (userOpt.isPresent()) {
-            HotelUser user = userOpt.get();
-            model.addAttribute("user", user);
-
-            int loyaltyPoints = bookingService.calculateLoyaltyPoints(user.getId());
-            model.addAttribute("loyaltyPoints", loyaltyPoints);
-        }
-        return "layout/connectedLayout";
-    }
 }
